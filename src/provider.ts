4ff748611a59ddd76cfd1780620bd92eb201ed94
ashleigh <ashleighsimonelli@gmail.com>
2024-03-01 09:08:37 +0000

    chore: resolve ts-ignore issue

--- src/provider.ts
+++ src/provider.ts
@@ -8,6 +8,6 @@ export const isValueProvider = (provider: Provider): provider is ValueProvider =
   provider.hasOwnProperty('token') && provider.hasOwnProperty('useValue')
 export const isTokenProvider = (provider: Provider): provider is TokenProvider =>
   provider.hasOwnProperty('token') && provider.hasOwnProperty('useClass')
-// @ts-ignore
 export const isConstructorProvider = (provider: Provider): provider is constructor<any> =>
+  // @ts-ignore
   !!provider.prototype && !!provider.prototype.constructor.name
